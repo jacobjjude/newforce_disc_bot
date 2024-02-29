dd5d7ec394fc7e2d41ebe3fafea2ac0000abea11
Jacob <jacobj.jude@outlook.com>
2024-02-29 16:20:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace main_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
